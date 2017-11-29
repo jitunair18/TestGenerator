15fc54edf6e3a68c2f95a0b9f3799e41f72e1ea8
Shreejit Nair <Shreejit.Nair1@ibm.com@shreejits-mbp.cambridge.ibm.com>
2017-11-29 11:03:47 -0500

    added master.sh script

---
+++ master.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# pass group as variable to master script
+java -jar swaggerParserUtility-0.01-combined-with-tests.jar $1
+
+# use exit status as exit code for program
+STATUS=$?
+
+pathname=$1
+echo $pathname
+dirName=$(basename $pathname)
+echo $dirName
+echo $STATUS
+# echo mylogReport=./logfile.log  >>  "%qm_AttachmentsFile%" 
+exit $STATUS
